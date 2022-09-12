0a0d0b5342e4a17b51d95e834e082a5df6c158c6
Abdul Munim <iam@munim.net>
2022-09-12 22:13:09 +0200

    Moved Inter font load to page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './style.css';
 document.querySelector(".my-em").href = window.atob("bWFpbHRvOmhlbGxvQG11bmltLm5ldA==");
 document.querySelector(".my-tg").href = window.atob("aHR0cHM6Ly90Lm1lL211bmltMzE2");
 
-document.querySelector(".main").style.display = "";
+document.querySelector(".loading").classList.remove("loading");
