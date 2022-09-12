af5075bc917c7c5646143ace8ea77279574dc7ad
Abdul Munim <iam@munim.net>
2022-09-12 21:51:44 +0200

    Hide content until loaded

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './style.css';
 
 document.querySelector(".my-em").href = window.atob("bWFpbHRvOmhlbGxvQG11bmltLm5ldA==");
 document.querySelector(".my-tg").href = window.atob("aHR0cHM6Ly90Lm1lL211bmltMzE2");
+
+document.querySelector(".main").style.display = "";
