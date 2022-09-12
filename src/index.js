76877b18c7e13c25604dcce68316b5731b202d39
Abdul Munim <iam@munim.net>
2022-09-12 21:33:15 +0200

    First version of the munim.net

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+
+document.querySelector(".my-em").href = window.atob("bWFpbHRvOmhlbGxvQG11bmltLm5ldA==");
+document.querySelector(".my-tg").href = window.atob("aHR0cHM6Ly90Lm1lL211bmltMzE2");
