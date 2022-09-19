a19fe141c294d55e54bf513883e36f95b890ee12
Abdul Munim <iam@munim.net>
2022-09-19 14:00:06 +0200

    Added page load fade-in transition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './style.css';
 
-document.querySelector(".my-em").href = window.atob("bWFpbHRvOmhlbGxvQG11bmltLm5ldA==");
-document.querySelector(".my-tg").href = window.atob("aHR0cHM6Ly90Lm1lL211bmltMzE2");
+document.addEventListener("DOMContentLoaded", () => {
+    var replacers = document.querySelectorAll('[data-replace]');
+    replacers.forEach(replItem => {
+        let replaceClasses = JSON.parse(replItem.dataset.replace.replace(/'/g, '"'));
+        Object.keys(replaceClasses).forEach(key => {
+            replItem.classList.remove(key);
+            var replaceObj = replaceClasses[key]
+            if (Array.isArray(replaceObj)) {
+                replaceObj.forEach(obj => replacers[i].classList.add(obj));
+            }
+            else if (typeof replaceObj === 'string') {
+                replItem.classList.add(replaceClasses[key]);
+            }
+        });
+    });
 
-document.querySelector(".loading").classList.remove("loading");
+    document.querySelector(".my-em").href = window.atob("bWFpbHRvOmhlbGxvQG11bmltLm5ldA==");
+    document.querySelector(".my-tg").href = window.atob("aHR0cHM6Ly90Lm1lL211bmltMzE2");
+});
+
